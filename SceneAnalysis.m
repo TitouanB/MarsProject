1171b5a13875e771f0aece8ee46f23bc7f6b398f
TitouanB <titouan.boulmier@ensta-bretagne.org>
2015-11-12 17:08:13 +0100

    matalab continué

--- SceneAnalysis.m
+++ SceneAnalysis.m
@@ -6,6 +6,10 @@ d2r=pi/180;
 h=6.6263*10^-34; %Planck's constant
 c=3*10^8; %Velocity if light
 k=1.38062*10^-23; %Boltzmann's constant
+AtargetMin=0.01; % surface's target min [m²]
+AtargetMax=0.1; % surface's target max [m²]
+shutterTime=10^-3; % shutter time [s]
+
 
 %Lens system
 Dsr=0; %Effective lens entrance aperture
@@ -28,14 +32,21 @@ IrradianceSunMin=IrradianceSun*sin(angleSunMin*d2r);
 
 %Target properties
 alphaMin=0.05; %albedo min
-alphaMax=80; %albedo max
+alphaMax=0.45; %albedo max
 
 thetaMin=10; %angle between sun's beam and tagert's normal
 thetaMax=50;
 
 %BRDF : 1/10 Glossy and 9/10 Lambertian
-RadianceTargetMin = IrradianceSun*alpha*cos(thetaMax)*(1/10+9/(10*pi));
-RadianceTargetMax = IrradianceSun*alpha*cos(thetaMin)*(1/10+9/(10*pi));
+RadianceTargetMin = IrradianceSunMin*alphaMin*cos(thetaMax*d2r)*(1/10+9/(10*pi)); % []
+RadianceTargetMax = IrradianceSunMax*alphaMax*cos(thetaMin*d2r)*(1/10+9/(10*pi));
+
+LuminousPowerTowardCamMin = RadianceTargetMin*AtargetMin; %[W]
+LuminousPowerTowardCamMax = RadianceTargetMax*AtargetMax; %[W]
+
+% Noise photons per shutter time
+noiseMin=LuminousPowerTowardCamMin*shutterTime/(h*c/lamdaDEL); %[]
+
 
 
 
