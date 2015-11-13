09bffd1d9e2eadf6b6895a0e44ab118da2410a6d
Katleen Blanchet <Kate@Katleens-MacBook-Pro.local>
2015-11-13 14:29:37 +0100

    Maj script

--- SceneAnalysis.m
+++ SceneAnalysis.m
@@ -6,15 +6,18 @@ d2r=pi/180;
 h=6.6263*10^-34; %Planck's constant
 c=3*10^8; %Velocity if light
 k=1.38062*10^-23; %Boltzmann's constant
-AtargetMin=0.01; % surface's target min [m²]
-AtargetMax=0.1; % surface's target max [m²]
-shutterTime=10^-3; % shutter time [s]
 
+
+AtargetMin=0.01; % surface's target min [m2]
+AtargetMax=0.1; % surface's target max [m2]
+shutterTime=100*10^-3; % shutter time [s]
+CCDqe=0.4;
 
 %Lens system
-Dsr=0; %Effective lens entrance aperture
+Dsr=0.005; %Effective lens entrance aperture
 EFL=0; %Effective focal length
-alphaLens=0; %Pass ban efficiency of lens systm
+alphaLens=0.98; %Pass ban efficiency of lens systm
+r=3; %[2,4]
 
 
 
@@ -28,6 +31,7 @@ IrradianceSunMax=IrradianceSun*sin(angleSunMax*d2r);
 %at min work hour
 angleSunMin = 10; %arbitrary
 IrradianceSunMin=IrradianceSun*sin(angleSunMin*d2r);
+lambda=550 %[400,700]
 
 
 %Target properties
@@ -38,15 +42,21 @@ thetaMin=10; %angle between sun's beam and tagert's normal
 thetaMax=50;
 
 %BRDF : 1/10 Glossy and 9/10 Lambertian
-RadianceTargetMin = IrradianceSunMin*alphaMin*cos(thetaMax*d2r)*(1/10+9/(10*pi)); % []
-RadianceTargetMax = IrradianceSunMax*alphaMax*cos(thetaMin*d2r)*(1/10+9/(10*pi));
+RadianceTargetMin = IrradianceSunMin*alphaMin*(1/10+cos(thetaMax*d2r)*(9/(10*pi))); % []
+RadianceTargetMax = IrradianceSunMax*alphaMax*(1/10+cos(thetaMin*d2r)*(9/(10*pi)));
 
 LuminousPowerTowardCamMin = RadianceTargetMin*AtargetMin; %[W]
 LuminousPowerTowardCamMax = RadianceTargetMax*AtargetMax; %[W]
 
+
 % Noise photons per shutter time
-noiseMin=LuminousPowerTowardCamMin*shutterTime/(h*c/lamdaDEL); %[]
+noiseMin=LuminousPowerTowardCamMin*shutterTime/(h*c/lambda); %[]
+noiseMax=LuminousPowerTowardCamMax*shutterTime/(h*c/lambda); %[]
 
+%number of noise photons to Lens
+nphotnoiseCCDMin=(pi*(Dsr/2)^2)/(2*pi*r^2)*noiseMin;
+nphotnoiseCCDMax=(pi*(Dsr/2)^2)/(2*pi*r^2)*noiseMax;
 
-
-
+%Number of noise electrons registered by CCD
+nenCCDMin=nphotnoiseCCDMin*CCDqe*alphaLens;
+nenCCDMax=nphotnoiseCCDMax*CCDqe*alphaLens;
