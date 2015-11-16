bd9580ee7ace187d0f17d901a52bf9e8fc7efee3
Katleen Blanchet <Kate@Katleens-MacBook-Pro.local>
2015-11-16 14:00:54 +0100

    Update script + datasheet

--- SceneAnalysis.m
+++ SceneAnalysis.m
@@ -11,18 +11,49 @@ AtargetMin=0.01; % surface's target min [m2]
 AtargetMax=0.1; % surface's target max [m2]
 shutterTime=100*10^-3; % shutter time [s]
 
+lambda=[400 500 600 700]*10^(-9); % [400,700] nm
+
 % CCD sensor
-imageSectionHeight = 12.288; 
+imageSectionHeight = 12.288*10^(-3); % m
 CCDqe=[0.09 0.28 0.22 0.135]; % wavelengths = [400 500 600 700] nm
 
 % Lens system
 Dsr=0.005; %Effective lens entrance aperture
 EFL=0; %Effective focal length
 alphaLens=0.98; %Pass ban efficiency of lens systm
-r=3; %[2,4]
-DoF = 4-2;
-FOV = 30; %30?x30?
-f = imageSectionHeight/2/tan(FOV*d2r);
+r=2; %[1,2]
+DoF = 2-1; % m
+objectSize = 2; % m
+FOVr = atan(objectSize/2/r);
+FOVd = FOVr*r2d;
+EFL = imageSectionHeight/2/tan(FOVr); % m
+
+% Confusion
+rf = 1;
+f = 1/(1/rf+1/EFL);
+Dsrtmp=[1:0.1:5]*10^(-3);
+figure;
+subplot(1,2,1);
+DoC=Dsrtmp*abs(r-rf)/r*f/(rf-f); % Diameter of Confusion
+plot(Dsrtmp, DoC);
+
+Difspot = zeros(4,length(Dsrtmp));
+hold on
+for i=1:4
+    Difspot(i,:) = 2*EFL*tan(1.22*lambda(i)./Dsrtmp); % Diffraction spot size on CCD
+    plot(Dsrtmp, Difspot);
+end
+legend('DoC (m)', 'Difspot (m) lambda = 400 nm', 'Difspot (m) lambda = 500 nm', 'Difspot (m) lambda = 600 nm', 'Difspot (m) lambda = 700 nm')
+xlabel('Dsr (m)')
+
+subplot(1,2,2);
+hold on
+for i=1:4
+    plot(Dsrtmp, Difspot(i,:)+DoC);
+end
+xlabel('Dsr (m)')
+ylabel('DoC + Difspot (m)')
+legend('lambda = 400 nm', 'lambda = 500 nm', 'lambda = 600 nm', 'lambda = 700 nm')
 
 % Reflectance map
 IrradianceSun = 530;
@@ -35,8 +66,6 @@ IrradianceSunMax=IrradianceSun*sin(angleSunMax*d2r);
 %at min work hour
 angleSunMin = 10; %arbitrary
 IrradianceSunMin=IrradianceSun*sin(angleSunMin*d2r);
-lambda=550 %[400,700]
-
 
 %Target properties
 alphaMin=0.05; %albedo min
@@ -54,13 +83,13 @@ LuminousPowerTowardCamMax = RadianceTargetMax*AtargetMax; %[W]
 
 
 % Noise photons per shutter time
-noiseMin=LuminousPowerTowardCamMin*shutterTime/(h*c/lambda); %[]
-noiseMax=LuminousPowerTowardCamMax*shutterTime/(h*c/lambda); %[]
+noiseMin=LuminousPowerTowardCamMin*shutterTime./(h*c./lambda); %[]
+noiseMax=LuminousPowerTowardCamMax*shutterTime./(h*c./lambda); %[]
 
 %number of noise photons to Lens
-nphotnoiseCCDMin=(pi*(Dsr/2)^2)/(2*pi*r^2)*noiseMin;
-nphotnoiseCCDMax=(pi*(Dsr/2)^2)/(2*pi*r^2)*noiseMax;
+nphotnoiseCCDMin=(pi*(Dsr/2)^2)/(2*pi*r^2).*noiseMin;
+nphotnoiseCCDMax=(pi*(Dsr/2)^2)/(2*pi*r^2).*noiseMax;
 
 %Number of noise electrons registered by CCD
-nenCCDMin=nphotnoiseCCDMin*CCDqe*alphaLens;
-nenCCDMax=nphotnoiseCCDMax*CCDqe*alphaLens;
+nenCCDMin=nphotnoiseCCDMin.*CCDqe*alphaLens;
+nenCCDMax=nphotnoiseCCDMax.*CCDqe*alphaLens;
